90ba36075b8d7efb0fe805339143aaee4bc54014
Ivan Rusanov <ivan.b.rusanov@gmail.com>
2021-05-06 00:41:41 +0300

    Port modules info to legacy app

--- WPF-no-themes/Windows/AboutDialog.xaml.cs
+++ WPF-no-themes/Windows/AboutDialog.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.Reflection;
 using System.Windows;
@@ -40,12 +41,49 @@ namespace ZenTimings.Windows
             this.labelVersion.Text = string.Format("Version {0}L", AssemblyVersion);
             this.labelCopyright.Text = AssemblyCopyright;
             this.labelCompanyName.Text = AssemblyDescription;
+
+            // List of all modules, there might be more DLL files in the directory
+            string[] files =
+            {
+                "AdonisUI.ClassicTheme.dll",
+                "AdonisUI.dll",
+                "AutoUpdater.NET.dll",
+                "inpoutx64.dll",
+                "WinIo32.dll",
+                "WinRing0x64.dll",
+                "ZenStates-Core.dll",
+            };
+            var appModules = new List<KeyValuePair<string, string>>();
+            
+            foreach (var file in files)
+            {
+                var version = "missing";
+
+                try
+                {
+                    FileVersionInfo fileVersionInfo = FileVersionInfo.GetVersionInfo(file);
+                    version = fileVersionInfo.FileVersion.Replace(", ", ".");
+                }
+                catch (Exception ex) 
+                {
+                    // Do Nothing 
+                }
+
+                appModules.Add(new KeyValuePair<string, string>(file.Replace(".dll", ""), version));
+            }
+
+            Modules.ItemsSource = appModules;
         }
 
         private void Hyperlink_RequestNavigate(object sender, System.Windows.Navigation.RequestNavigateEventArgs e)
         {
             Process.Start(new ProcessStartInfo(e.Uri.AbsoluteUri));
             e.Handled = true;
+        }
+
+        private void CheckUpdateBtn_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            Updater.CheckForUpdate(true);
         }
     }
 }
