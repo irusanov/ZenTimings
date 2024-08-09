1aeb5b72aea671d3a6b510aa309455da283d5784
Ivan Rusanov <ivan.b.rusanov@gmail.com>
2024-08-09 22:12:11 +0300

    update

--- WPF/Properties/BuildNumberTemplate.cs
+++ WPF/Properties/BuildNumberTemplate.cs
@@ -5,5 +5,5 @@
 
 using System.Reflection;
 
-[assembly: AssemblyVersion("1.33.1436")]
-[assembly: AssemblyFileVersion("1.33.1436")]
+[assembly: AssemblyVersion("1.33.1438")]
+[assembly: AssemblyFileVersion("1.33.1438")]
