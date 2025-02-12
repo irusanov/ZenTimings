fc34983637aa17bde1331ca78332c812a1a026c1
Ivan Rusanov <ivan.b.rusanov@gmail.com>
2025-02-12 15:09:33 +0200

    Add AGESA version line if available

--- WPF/Properties/BuildNumberTemplate.cs
+++ WPF/Properties/BuildNumberTemplate.cs
@@ -5,5 +5,5 @@
 
 using System.Reflection;
 
-[assembly: AssemblyVersion("1.35.1592")]
-[assembly: AssemblyFileVersion("1.35.1592")]
+[assembly: AssemblyVersion("1.36.1599")]
+[assembly: AssemblyFileVersion("1.36.1599")]
