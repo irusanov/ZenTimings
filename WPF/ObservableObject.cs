6a8078b49a5a7e3e7a92a383f5ac6251bfa14e31
Ivan Rusanov <ivan.b.rusanov@gmail.com>
2025-10-22 17:48:29 +0300

    add observable object class

---
+++ WPF/ObservableObject.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+
+namespace ZenTimings
+{
+public abstract class ObservableObject : INotifyPropertyChanged
+{
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    protected bool SetProperty<T>(ref T storage, T value, [CallerMemberName] string propertyName = "")
+    {
+        if (Equals(storage, value)) return false;
+        storage = value;
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        return true;
+    }
+}
+}
