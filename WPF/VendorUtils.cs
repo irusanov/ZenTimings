ecf4ab94955b86eafec8cb434831f92eb71a719d
Ivan Rusanov <ivan.b.rusanov@gmail.com>
2026-02-05 17:49:32 +0200

    Update vendor logos

--- WPF/VendorUtils.cs
+++ WPF/VendorUtils.cs
@@ -1,4 +1,4 @@
-﻿using ZenStates.Core;
+using ZenStates.Core;
 using ZenStates.Core.DRAM;
 
 namespace ZenTimings
@@ -44,11 +44,17 @@ namespace ZenTimings
 
         internal static string GetMotherboardLogo(SystemInfo systemInfo)
         {
-            if (IsRogMotherboard(systemInfo)) return "ROGLogo";
+            if (IsRogMotherboard(systemInfo)) return "rogLogo";
             if (IsAYWMotherboard(systemInfo)) return "aywLogo";
             if (IsProArtMotherboard(systemInfo)) return "proartLogo";
+            //if (IsMegMotherboard(systemInfo)) return "megLogo";
 
             return null;
+        }
+
+        private static bool IsMegMotherboard(SystemInfo info)
+        {
+            return (info.MbVendor.ToLower().StartsWith("msi") || info.MbVendor.StartsWith("Microstar International")) && info.MbName.ToLower().StartsWith("meg");
         }
 
         internal static string GetMemoryModuleLogo(MemoryModule module)
@@ -65,6 +71,10 @@ namespace ZenTimings
             {
                 return "biwinLogo";
             }
+            else if (IColorfulModule(module))
+            {
+                return "igameLogo";
+            }
 
             return null;
         }
@@ -72,24 +82,48 @@ namespace ZenTimings
         internal static bool IsGSkillModule(MemoryModule module)
         {
             return module.Manufacturer.ToLowerInvariant().Contains("skill")
-                || module.PartNumber.ToLowerInvariant().StartsWith("f5-")
-                || module.PartNumber.ToLowerInvariant().StartsWith("f4-");
+                || module.PartNumber.ToLowerInvariant().Trim().StartsWith("f5-")
+                || module.PartNumber.ToLowerInvariant().Trim().StartsWith("f4-");
         }
 
         internal static bool IsBiwinModule(MemoryModule module)
         {
             return module.Manufacturer.ToLowerInvariant().Contains("biwin")
-                || module.PartNumber.ToLowerInvariant().StartsWith("bm")
-                || module.PartNumber.ToLowerInvariant().StartsWith("bx")
-                || module.PartNumber.ToLowerInvariant().StartsWith("ba")
-                || module.PartNumber.ToLowerInvariant().StartsWith("ocl")
-                || module.PartNumber.ToLowerInvariant().StartsWith("ocb");
+                || module.PartNumber.ToLowerInvariant().Trim().StartsWith("bm")
+                || module.PartNumber.ToLowerInvariant().Trim().StartsWith("bx")
+                || module.PartNumber.ToLowerInvariant().Trim().StartsWith("ba")
+                || module.PartNumber.ToLowerInvariant().Trim().StartsWith("ocl")
+                || module.PartNumber.ToLowerInvariant().Trim().StartsWith("ocb");
         }
 
         internal static bool IsOriginCodeModule(MemoryModule module)
         {
-            return module.PartNumber.ToLowerInvariant().StartsWith("ocl")
-                || module.PartNumber.ToLowerInvariant().StartsWith("ocb");
+            return module.PartNumber.ToLowerInvariant().Trim().StartsWith("ocl")
+                || module.PartNumber.ToLowerInvariant().Trim().StartsWith("ocb");
+        }
+
+        internal static bool IColorfulModule(MemoryModule module)
+        {
+            //return module.Manufacturer.ToLowerInvariant().Contains("colorful")
+            //    && module.PartNumber.ToLowerInvariant().Trim().StartsWith("ig");
+            return module.PartNumber.ToLowerInvariant().Trim().StartsWith("ig");
+        }
+
+        internal static string GetCpuNameString(SystemInfo info)
+        {
+            try
+            {
+                var name = info.CpuName;
+                if (name.Contains("Eng Sample"))
+                {
+                    return $"{name} | {info.CodeName} | 0x{info.CpuId:X6}";
+                }
+                return name;
+            }
+            catch
+            {
+                return "Error getting CPU name";
+            }
         }
     }
 }
