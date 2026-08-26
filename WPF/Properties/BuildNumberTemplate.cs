690b636f23a3aa7f3c617b47573681d5acf72396
Ivan Rusanov <ivan.b.rusanov@gmail.com>
2026-08-26 23:16:26 +0300

    add icons to sections in options dialog

--- WPF/Properties/BuildNumberTemplate.cs
+++ WPF/Properties/BuildNumberTemplate.cs
@@ -3,4 +3,4 @@
 using System.Reflection;
 [assembly: AssemblyVersion("140.516")]
 [assembly: AssemblyFileVersion("140.516")]
-[assembly: AssemblyInformationalVersion("140.516+500dad2")]
+[assembly: AssemblyInformationalVersion("140.516+060f275")]
