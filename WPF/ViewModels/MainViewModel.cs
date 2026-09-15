56f79e5d094bbc8dc6c2f11ec4ee94cfefac6bf8
Ivan Rusanov <ivan.b.rusanov@gmail.com>
2026-09-15 06:18:52 +0300

    Update ZenStates-Core submodule

--- WPF/ViewModels/MainViewModel.cs
+++ WPF/ViewModels/MainViewModel.cs
@@ -36,12 +36,8 @@ namespace ZenTimings.ViewModels
             get => _timings;
             set
             {
-                // Not using SetProperty's equality short-circuit here: MemoryFrequency must be
-                // recomputed from the new timings object every time this is assigned, even if a
-                // future caller happened to pass back an equal instance.
-                _timings = value;
-                MemoryFrequency = value.Frequency;
-                OnPropertyChanged();
+                if (SetProperty(ref _timings, value) && value != null)
+                    MemoryFrequency = value.Frequency;
             }
         }
         public AppSettings Settings { get; }
@@ -292,6 +288,7 @@ namespace ZenTimings.ViewModels
                 TotalCapacity = mockData.TotalCapacity;
                 CodeName = mockData.CpuInfo.codeName;
                 PowerTable = mockData.PowerTable;
+                MemoryFrequencyString = $"{(PowerTable.MCLK * 2)} MT/s";
             }
             else
             {
