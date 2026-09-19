26e8f511ed7cf5a83a0e5d53898356583c6ae4dc
Ivan Rusanov <ivan.b.rusanov@gmail.com>
2026-09-20 02:43:24 +0300

    Style updates. Move copy buttons consistently on the right

--- WPF/Windows/SpdInfoWindow.xaml.cs
+++ WPF/Windows/SpdInfoWindow.xaml.cs
@@ -380,6 +380,8 @@ namespace ZenTimings.Windows
                 ItemsSource = rows
             };
 
+            grid.SetResourceReference(DataGrid.RowStyleProperty, "SpdDataGridRowStyle");
+
             grid.Columns.Add(new DataGridTextColumn
             {
                 Width = new DataGridLength(240),
@@ -393,11 +395,13 @@ namespace ZenTimings.Windows
                 ElementStyle = valueStyle
             });
 
-            return new TabItem
+            var tab = new TabItem
             {
                 Header = header,
                 Content = grid
             };
+
+            return tab;
         }
 
         private static string FormatValue(object value)
